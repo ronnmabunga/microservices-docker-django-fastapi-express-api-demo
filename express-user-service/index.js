733b37a7d01070ac91f9ea9a47db9f3c41d6dd00
ronnmabunga <ronnmabunga@gmail.com>
2024-11-15 23:12:58 +0800

    Add user service (Express.js API)

---
+++ express-user-service/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/auth", (req, res) => {
+    res.status(200).send({ message: "User Authentication Service" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Express Auth & Profile Service listening at http://localhost:${PORT}`);
+});
